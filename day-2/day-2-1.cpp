57cabb249f5dfd1246dfff17d9455e1c310f88a9
cakers <chris.j.akers@gmail.com>
2024-12-15 13:24:29 +0000

    2024-12-15 1324

--- day-2/day-2-1.cpp
+++ day-2/day-2-1.cpp
@@ -23,19 +23,33 @@ vector<int> getVectorOfNumsFromString(const string s) {
     return returnVector;
 }
 
+vector<vector<int>> loadReports(const string inputFilePath) {
+    ifstream inputFile(inputFilePath);
+    string line;
+    string num;
+    vector<vector<int>> newReport;
+    while (std::getline(inputFile, line)) {
+        newReport.push_back(getVectorOfNumsFromString(line));
+    }
+    return newReport;
+}
+
 bool checkReport(const vector<int> &report) {
     if (report[0] == report[1]) {
         return false;
     }
 
-    auto gt = [](int a, int b) { return a > b; };
-    auto lt = [](int a, int b) { return a < b; };
-
     std::function<bool(int a, int b)> evaluator;
     if (report[0] < report[1]) {
-        evaluator = [](int a, int b) { return a < b; };
+        evaluator = [](int a, int b) {
+            int difference = abs(a - b);
+            return (difference > 0 && difference <= 3) && a < b; 
+        };
     } else {
-        evaluator = [](int a, int b) { return a > b; };
+        evaluator = [](int a, int b) { 
+            int difference = abs(a - b);
+            return (difference > 0 && difference <= 3) && a > b; 
+        };
     }
 
     int numberOfLevels = report.size();    
@@ -49,26 +63,12 @@ bool checkReport(const vector<int> &report) {
     return true;    
 }
 
-vector<vector<int>> loadReports(const string inputFilePath) {
-    ifstream inputFile(inputFilePath);
-    string line;
-    string num;
-    vector<vector<int>> newReport;
-    while (std::getline(inputFile, line)) {
-        newReport.push_back(getVectorOfNumsFromString(line));
-    }
-    return newReport;
-}
-
-
 int main() {
     vector<vector<int>> reportsVector = loadReports("./success.txt");
 
     int correctResults = 0;
-
     for (auto report : reportsVector) {
         if (checkReport(report)) correctResults++;
     }
-
     cout << correctResults << " safe reports." << endl;
 }
