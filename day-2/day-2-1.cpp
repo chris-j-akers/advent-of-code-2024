0bd89617b139c0be42e642cf1d824629356706b8
cakers <chris.j.akers@gmail.com>
2024-12-15 12:56:58 +0000

    refactor day-2-1

--- day-2/day-2-1.cpp
+++ day-2/day-2-1.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<fstream>
 #include<string>
-#include<list>
+#include<vector>
 #include<sstream>
 #include<functional>
 
@@ -9,74 +9,66 @@ using namespace std;
 
 // I think there are 332 safe reports.
 
-string reportToString(const list<int> &report) {
-    ostringstream os;
-    for (auto level: report) {
-        os << level << ' ';
-    }
-    return os.str();
-}
-
-
-list<int> getListOfLevelsFromString(const string s) {
-    list<int> levels;    
-    int stringStartPos = 0;
-    int stringCurrPos = 0;
+vector<int> getVectorOfNumsFromString(const string s) {
+    vector<int> returnVector;    
+    int startPos = 0;
+    int currPos = 0;
     string levelStr;
-    while(stringCurrPos < s.size()) {
-        stringCurrPos = s.find(" ", stringStartPos);
-        levelStr = s.substr(stringStartPos, stringCurrPos-stringStartPos);        
-        levels.push_back(stoi(levelStr));
-        stringStartPos = stringCurrPos+1;
+    while(currPos < s.size()) {
+        currPos = s.find(" ", startPos);
+        levelStr = s.substr(startPos, currPos-startPos);        
+        returnVector.push_back(stoi(levelStr));
+        startPos = currPos+1;
     }
-    return levels;
+    return returnVector;
 }
 
+bool checkReport(const vector<int> &report) {
+    if (report[0] == report[1]) {
+        return false;
+    }
 
-// while (list1Iterator != list1.end() && list2Iterator != list2.end()) {
+    auto gt = [](int a, int b) { return a > b; };
+    auto lt = [](int a, int b) { return a < b; };
 
-bool checkReport(const list<int> &report, const std::function<bool(int a, int b)> eval) {
+    std::function<bool(int a, int b)> evaluator;
+    if (report[0] < report[1]) {
+        evaluator = [](int a, int b) { return a < b; };
+    } else {
+        evaluator = [](int a, int b) { return a > b; };
+    }
+
     int numberOfLevels = report.size();    
     int difference = 0;
-    for (auto reportsIter = report.begin(); reportsIter -1; i++) {
+    for (int i=0; i<numberOfLevels -1; i++) {
         difference = abs(report[i] - report[i+1]);
-        if (!(eval(report[i], report[i+1]) && difference > 0 && difference <= 3)) {
+        if (!(evaluator(report[i], report[i+1]) && difference > 0 && difference <= 3)) {
             return false;
         }
     }
     return true;    
 }
 
-vector<list<int>> loadReports(const string inputFilePath) {
+vector<vector<int>> loadReports(const string inputFilePath) {
     ifstream inputFile(inputFilePath);
     string line;
     string num;
-    vector<list<int>> newReport;
+    vector<vector<int>> newReport;
     while (std::getline(inputFile, line)) {
-        newReport.push_back(getListOfLevelsFromString(line));
+        newReport.push_back(getVectorOfNumsFromString(line));
     }
     return newReport;
 }
 
 
 int main() {
-    vector<list<int>> reportsList = loadReports("./input.txt");
-    
-    for (list<int> l : reportsList) {
-        cout << "got list: " << reportToString(l) << endl;
+    vector<vector<int>> reportsVector = loadReports("./success.txt");
+
+    int correctResults = 0;
+
+    for (auto report : reportsVector) {
+        if (checkReport(report)) correctResults++;
     }
 
-    // bool result;
-    // int correctResults = 0;
-
-    // for (auto report : reportsVector) {
-    //     if (report[0] < report[1]) {
-    //         result = checkReport(report, [](int a, int b) { return a < b; });
-    //     } 
-    //     else {
-    //         result = checkReport(report, [](int a, int b) { return a > b; });
-    //     }
-    //     if (result) correctResults++;
-    // }
-    // cout << "I think there are " << correctResults << " safe reports." << endl;
+    cout << correctResults << " safe reports." << endl;
 }
